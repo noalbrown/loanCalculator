9c4e6104bdd0900240b1db727a954f8a07ffa75a
Noal Brown <noalbrown1@gmail.com>
2020-11-01 10:00:25 -0600

    finished

--- app.js
+++ app.js
@@ -1,54 +1,80 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
 
-// Calculate results
-function calculateResults(e) {
-  console.log('calculating')
-  // UI variables
-  const amount = document.getElementById('amount')
-  const interest = document.getElementById('interest')
-  const years = document.getElementById('years')
-  const monthlyPayment = document.getElementById('monthly-payment')
-  const totalPayment = document.getElementById('total-payment')
-  const totalInterest = document.getElementById('total-interest')
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
 
-  const principal = parseFloat(amount.value)
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults() {
+  console.log('Calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12
+  const calculatedPayments = parseFloat(years.value) * 12;
 
-  // Compute Monthly Payments
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments)
-  const monthly = (principal * x * calculatedInterest) / (x - 1)
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
   if (isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2)
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2)
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
-    showError('Check your numbers')
+    showError('Please check your numbers');
   }
-
-  e.preventDefault()
 }
 
-// Error function
+// Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
-  const errorDiv = document.createElement('div')
+  const errorDiv = document.createElement('div');
+
   // Get elements
-  const card = document.querySelector('.card')
-  const heading = document.querySelector('.heading')
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
   // Add class
-  errorDiv.className = 'alert alert-danger'
+  errorDiv.className = 'alert alert-danger';
+
   // Create text node and append to div
-  errorDiv.appendChild(document.createTextNode(error))
-  // Insert Error above heading
-  card.insertBefore(errorDiv, heading)
-  // Clear error message
-  setTimeout(clearError, 3000)
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
 }
 
-// clear error
+// Clear error
 function clearError() {
-  document.querySelector('.alert').remove()
+  document.querySelector('.alert').remove();
 }
